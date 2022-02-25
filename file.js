e9b31ab0ed71fc8e0b15edc2734879de88eb930f
Adam Lehechka <ajlehechka@gmail.com>
2022-02-25 15:00:51 -0600

    add js file with comments

---
+++ file.js
@@ -0,0 +1,9 @@
+// TODO: JIRA-123 do the first thing
+console.log('test'); // TODO: JIRA-123 do the second thing
+//TODO: JIRA-123 do the third thing
+
+// TODO: [JIRA-123] do the fourth thing
+
+// TODO: DEF-456 - do not do the fifth thing
+
+// TODO: do not do the sixth thing
